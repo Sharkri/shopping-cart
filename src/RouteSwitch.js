05d01a1e30b1212b406f41ed882e47e80476bf89
Sharkri <sharkdude955@gmail.com>
2022-11-07 15:16:10 -0600

    Add shop page

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Shop from "./components/Shop";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/shop" element={<Shop />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Shop.js
@@ -0,0 +1,9 @@
+function Shop() {
+  return (
+    <div className="shop">
+      <h1>Shop Page</h1>
+    </div>
+  );
+}
+
+export default Shop;

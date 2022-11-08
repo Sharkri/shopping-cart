9acbb8f032c3c03e31fb3fdc69de51db5f6b5379
Sharkri <sharkdude955@gmail.com>
2022-11-08 04:04:38 -0600

    Add links functionality in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,13 @@
-function Nav() {
-  return <nav></nav>;
+import { Link } from "react-router-dom";
+
+function Nav({ links }) {
+  return (
+    <nav>
+      {links.map((link) => (
+        <Link to={link.to}>{link.name}</Link>
+      ))}
+    </nav>
+  );
 }
 
 export default Nav;
---
+++ src/tests/Nav.test.js
@@ -0,0 +1,31 @@
+import Nav from "../components/Nav";
+import { render, screen } from "@testing-library/react";
+
+jest.mock("react-router-dom", () => ({
+  Link: (props) => (
+    <>
+      <div data-testid="name">{props.children}</div>
+      <div data-testid="to">{props.to}</div>
+    </>
+  ),
+}));
+
+const links = [
+  { name: "Home", to: "/" },
+  { name: "Shop", to: "/shop" },
+];
+
+describe("nav", () => {
+  it("should pass in correct props which are url and name", async () => {
+    render(<Nav links={links} />);
+
+    const names = screen.getAllByTestId("name");
+    const urls = screen.getAllByTestId("to");
+
+    expect(names[0].textContent).toMatch(/Home/i);
+    expect(urls[0].textContent).toMatch("/");
+
+    expect(names[1].textContent).toMatch(/Shop/i);
+    expect(urls[1].textContent).toMatch("/shop");
+  });
+});

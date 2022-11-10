38fb504215372cceadd658335cca3d70601866ae
Sharkri <sharkdude955@gmail.com>
2022-11-09 23:06:43 -0600

    Display nav on screen

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,10 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Nav from "./components/Nav";
 import Shop from "./components/Shop";
 
 const RouteSwitch = () => {
+  const links = [
+    { to: "/", name: "Home" },
+    { to: "/shop", name: "Shop" },
+  ];
+
   return (
     <BrowserRouter>
+      <Nav links={links} />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/shop" element={<Shop />} />

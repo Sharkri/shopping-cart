b753e2124fffbc9171abfa44ec1b5421acbbc06b
Sharkri <sharkdude955@gmail.com>
2022-11-10 03:11:38 -0600

    Organize css files into a "styles" folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/index.css";
 import RouteSwitch from "./RouteSwitch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));

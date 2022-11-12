c2128cc22a031f4130f66bf971ed431555aa93a5
Sharkri <sharkdude955@gmail.com>
2022-11-12 14:28:48 -0600

    Combine title and nav together in Header.js and display it to RouteSwitch.js

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,18 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Nav from "./components/Nav";
+import Header from "./components/Header";
 import Shop from "./components/Shop";
-import uniqid from "uniqid";
 
 const RouteSwitch = () => {
-  const links = [
-    { to: "/", name: "Home", id: uniqid() },
-    { to: "/shop", name: "Shop", id: uniqid() },
-  ];
-
   return (
     <BrowserRouter>
-      <Nav links={links} />
+      <Header />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/shop" element={<Shop />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import "../styles/Header.css";
+import Nav from "./Nav";
+import Title from "./Title";
+import uniqid from "uniqid";
 
 function Header() {
-  return <header></header>;
+  const links = [
+    { name: "Home", to: "/", id: uniqid() },
+    { name: "Shop", to: "/shop", id: uniqid() },
+  ];
+
+  return (
+    <header>
+      <Title title="idk" />
+      <Nav links={links} />
+    </header>
+  );
 }
 
 export default Header;

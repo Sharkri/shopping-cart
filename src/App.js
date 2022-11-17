1c94904b1b3c48af53c10b64ba16c26c881db487
Sharkri <sharkdude955@gmail.com>
2022-11-17 01:50:38 -0600

    Remove unused import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Shop from "./components/Shop";

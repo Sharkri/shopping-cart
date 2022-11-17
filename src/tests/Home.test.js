795451195bf8f9216486df16967365f6ecaed473
Sharkri <sharkdude955@gmail.com>
2022-11-16 21:58:40 -0600

    Assert link href is correct rather than asserting that clicking link will direct to page

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,21 +1,19 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import Home from "../components/Home";
+import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
 
 describe("homepage", () => {
-  it("should be able to navigate to shop", () => {
+  it("should link to correct href", () => {
     render(
       <BrowserRouter>
         <Home />
       </BrowserRouter>
     );
 
-    expect(window.location.href).toBe("http://localhost/");
-
-    const goToShop = screen.getByRole("link", { name: "Shop Now" });
-    userEvent.click(goToShop);
-
-    expect(window.location.href).toBe("http://localhost/shop");
+    expect(screen.getByRole("link", { name: "Shop Now" })).toHaveAttribute(
+      "href",
+      "/shop"
+    );
   });
 });
--- src/tests/Nav.test.js
+++ src/tests/Nav.test.js
@@ -1,7 +1,7 @@
 import Nav from "../components/Nav";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 
 describe("nav", () => {
   it("should navigate to href properly", async () => {
@@ -10,10 +10,10 @@ describe("nav", () => {
         <Nav />
       </BrowserRouter>
     );
-    expect(window.location.href).toBe("http://localhost/");
 
-    const shop = screen.getByRole("link", { name: "Shop" });
-    userEvent.click(shop);
-    expect(window.location.href).toBe("http://localhost/shop");
+    expect(screen.getByRole("link", { name: "Shop" })).toHaveAttribute(
+      "href",
+      "/shop"
+    );
   });
 });

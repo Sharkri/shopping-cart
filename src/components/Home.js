7e335d240b4d4b2f8c1afb0005867db02b5174ea
Sharkri <sharkdude955@gmail.com>
2022-11-16 19:20:13 -0600

    Add homepage top section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,23 @@
 import { Link } from "react-router-dom";
 import "../styles/Home.css";
+import homepage from "../images/homepage.jpg";
 
 function Home() {
   return (
     <div className="home">
-      <Link to="/shop" className="shop-now">
-        Shop Now
-      </Link>
+      <section className="top-section">
+        <img
+          src={homepage}
+          alt="living room with furniture"
+          className="homepage-img"
+        />
+        <div className="image-text-overlay">
+          <h1 className="image-text">Make room for better living</h1>
+          <Link to="/shop" className="shop-now">
+            Shop Now
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }

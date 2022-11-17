a547f8196464546d37c732cc21a713c67d94cf4a
Sharkri <sharkdude955@gmail.com>
2022-11-17 01:00:51 -0600

    Add ProductPage

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,1 +1,14 @@
-function ProductPage() {}
+import { Link } from "react-router-dom";
+
+export default function ProductPage({ product, onAddToCart }) {
+  return (
+    <div className="product-page">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <button onClick={() => onAddToCart(product)}>Add to cart</button>
+      <img src={product.image} alt={product.name} />
+      <Link to="/shop">Go back</Link>
+    </div>
+  );
+}
---
+++ src/tests/ProductPage.test.js
@@ -0,0 +1,60 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import ProductPage from "../components/ProductPage";
+import "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
+
+// 1. Show image and name
+// 2. Show additional details e.g. description
+// 3. Have a button to add to cart
+// 4. Add Button to go back
+// 5. idk
+
+jest.mock("react-router-dom", () => ({
+  Link: ({ to, children }) => <a href={to}>{children}</a>,
+}));
+
+const product = {
+  name: "Product Name",
+  description: "This product is cool.",
+  path: "/products/cool-product",
+  price: "$69.99",
+  image: "test-image.png",
+};
+
+it("should render name, description, price, and image", () => {
+  render(<ProductPage product={product} />);
+
+  expect(
+    screen.getByRole("heading", { name: "Product Name" })
+  ).toBeInTheDocument();
+
+  expect(screen.getByText("This product is cool.")).toBeInTheDocument();
+
+  expect(screen.getByText("$69.99")).toBeInTheDocument();
+
+  const image = screen.getByRole("img");
+  expect(image).toHaveAttribute("alt", "Product Name");
+  expect(image).toHaveAttribute("src", "test-image.png");
+});
+
+it("should call function on add to cart click", () => {
+  const mockAddToCart = jest.fn();
+  render(<ProductPage onAddToCart={mockAddToCart} product={product} />);
+
+  const addToCart = screen.getByRole("button", { name: /Add to cart/i });
+
+  userEvent.click(addToCart);
+
+  // Check if function was called with product
+  expect(mockAddToCart).toHaveBeenCalledWith(expect.objectContaining(product));
+});
+
+it("should be able to navigate back to shop", () => {
+  render(<ProductPage product={product} />);
+
+  expect(screen.getByRole("link", { name: /Go back/i })).toHaveAttribute(
+    "href",
+    "/shop"
+  );
+});

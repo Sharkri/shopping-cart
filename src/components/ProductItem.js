bf37b71bfbeb448b03f37adab61df23cf3a89b98
Sharkri <sharkdude955@gmail.com>
2022-11-21 23:05:21 -0600

    Style ProductItem and reorder its elements

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 export default function ProductItem({ product }) {
   return (
     <Link className="shop-item" to={product.path}>
-      <p>{product.name}</p>
-      <p>{`${product.prefix}${product.price}`}</p>
-      <img src={product.image} alt={product.name} className="product-image" />
+      <div className="product-image-container">
+        <img src={product.image} alt={product.name} className="product-image" />
+      </div>
+      <h2 className="shop-item-product-name">{product.name}</h2>
+      <p className="shop-item-product-price">{`${product.prefix}${product.price}`}</p>
     </Link>
   );
 }

3902353db0b63257e7504c85065967fc71e66538
Sharkri <sharkdude955@gmail.com>
2022-11-21 21:34:34 -0600

    Add amount in cart (not functionality)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,10 @@ function Nav() {
     <nav>
       <Link to="/">Home</Link>
       <Link to="/shop">Shop</Link>
-      <Link to="/cart">Cart</Link>
+      <Link to="/cart" className="cart-link">
+        <i className="fa-solid fa-cart-shopping" />
+        <span className="amount-in-cart">2</span>
+      </Link>
     </nav>
   );
 }

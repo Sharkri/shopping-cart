4df038d7eb0cbb72fea1f738366ce4c27c7b5122
Sharkri <sharkdude955@gmail.com>
2022-11-24 21:30:48 -0600

    Remove unused imports

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 export default function RouteSwitch({ Header, pages }) {
   return (
